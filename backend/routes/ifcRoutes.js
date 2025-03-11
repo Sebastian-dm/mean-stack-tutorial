a14d4244965374f0c990ddb7d94fd897fc73fe31
Sebastian Meier <sebastiandmeier@gmail.com>
2025-03-11 13:25:47 +0100

    clear current progress

--- backend/routes/ifcRoutes.js
+++
@@ -1,9 +0,0 @@
-// backend/routes/ifcRoutes.js
-const express = require('express');
-const { uploadIFC, compareIFC } = require('../controllers/ifcController');
-const router = express.Router();
-
-router.post('/upload', uploadIFC);
-router.post('/compare', compareIFC);
-
-module.exports = router;
