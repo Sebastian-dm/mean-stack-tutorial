c3e94c45e13234105bacf36390f35e52b77dce64
Sebastian Meier <sebastiandmeier@gmail.com>
2025-03-25 13:01:35 +0100

    Make user component dynamic

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,9 @@ import { UserComponent } from "./user/user.component";
 })
 export class AppComponent {
   title = 'udemy';
+  users = DUMMY_USERS;
+
+  onSelectUser(id: string){
+    console.log("Selected user with id: "+id);
+  }
 }
